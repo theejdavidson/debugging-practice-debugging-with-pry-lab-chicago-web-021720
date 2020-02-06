44befe6ed44b170f811660198e1401bebf86f12b
Ethan Davidson <theejdavidson@gmail.com>
2020-02-06 22:30:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -14,7 +14,7 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  crew.each {|crew_member| crew[crew_member] = "Hello #{crew_member}."}
 end
 
 def engage
